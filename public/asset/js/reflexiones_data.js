3452824c77819bef6af42ae7958575f3da5365e4
Chadwin123 <pilarchadwin@gmail.com>
2026-02-06 23:46:20 -0400

    Actualizacion automatica desde Web Admin

--- public/asset/js/reflexiones_data.js
+++ public/asset/js/reflexiones_data.js
@@ -1,5 +1,11 @@
 export const reflexionesData = [
     {
+        "id": 1770435976288,
+        "title": "Nigh",
+        "content": "Wena noche",
+        "date": "2026-02-07"
+    },
+    {
         "id": 1770435380713,
         "title": "LA KABRA",
         "content": "Ay no se, que pereza",
