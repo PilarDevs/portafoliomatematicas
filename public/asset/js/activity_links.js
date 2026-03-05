73b27ebf7377e61188fb7dd82b910749f61530fa
Chadwin123 <pilarchadwin@gmail.com>
2026-03-04 23:52:20 -0400

    Actualizacion automatica desde Web Admin

--- public/asset/js/activity_links.js
+++ public/asset/js/activity_links.js
@@ -3,12 +3,12 @@
 
 export const activityLinks = {
     "Actividad 1.1": "https://drive.google.com/drive/folders/1aspe2LkIe50OUhA4F7MatSPpvU6MnDUw?usp=sharing",
-    "Actividad 1.2": "#",
-    "Actividad 1.3": "#",
-    "Actividad 1.4": "#",
-    "Actividad 2.1": "#",
-    "Actividad 2.2": "#",
-    "Actividad 2.3": "#",
+    "Actividad 1.2": "https://drive.google.com/drive/folders/1Yz95y0FUB0pbPYdntxj4JQeldjnZgsj6?usp=sharing",
+    "Actividad 1.3": "https://drive.google.com/drive/folders/1EfvI7N6CSDZ-GWdgAyNcDh78alR0y1qq?usp=sharing",
+    "Actividad 1.4": "https://drive.google.com/drive/folders/1GmorUqRFRn1suk8yCpS2umwtBBnrAXnJ?usp=sharing",
+    "Actividad 2.1": "https://drive.google.com/drive/folders/1-Uc38MfdVX68q41b4RQKzs1aDeq8WKZK?usp=sharing",
+    "Actividad 2.2": "https://drive.google.com/drive/folders/15qThcNzOrnrpeh2YBTYTkC9vmby9hs6r?usp=sharing",
+    "Actividad 2.3": "https://drive.google.com/drive/folders/1k5Haml3N3Ep1wsMHl96-gS0_5wet_3V-?usp=sharing",
     "Actividad 3.1": "#",
     "Actividad 3.2": "#",
     "Actividad 3.3": "#",
