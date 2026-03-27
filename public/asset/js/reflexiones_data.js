cab8a463c171c3dc478de8d29e109d4b352c9f3b
Chadwin123 <pilarchadwin@gmail.com>
2026-03-26 23:49:59 -0400

    Actualizacion automatica desde Web Admin

--- public/asset/js/reflexiones_data.js
+++ public/asset/js/reflexiones_data.js
@@ -1,5 +1,11 @@
 export const reflexionesData = [
     {
+        "id": 1774583398774,
+        "title": "Semana 3",
+        "content": "Mas sucesiones, sigo sin retenerlas pero aqui entra el tema de analisis, entender situaciones y darles una solucion segun las indicaciones, me gusta",
+        "date": "2026-03-27"
+    },
+    {
         "id": 1774583271150,
         "title": "Semana 2",
         "content": "Un tema interesante las suceciones, pero no logro retenerlo, soy bruto",
