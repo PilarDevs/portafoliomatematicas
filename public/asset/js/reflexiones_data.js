ada9b7327df1aeadf2f918eee36ae05cd4f1c256
Chadwin123 <pilarchadwin@gmail.com>
2026-03-26 23:46:20 -0400

    Actualizacion automatica desde Web Admin

--- public/asset/js/reflexiones_data.js
+++ public/asset/js/reflexiones_data.js
@@ -1,5 +1,11 @@
 export const reflexionesData = [
     {
+        "id": 1774583174956,
+        "title": "Semana 1",
+        "content": "Mmh, muy interesante la semana, ojala poder recordarla en las pruebas nacionales",
+        "date": "2026-03-27"
+    },
+    {
         "id": 1770435380713,
         "title": "LA KABRA",
         "content": "Ay no se, que pereza",
