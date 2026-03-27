4669d36b89fd82b2861b9697bf599fb9bc1358db
Chadwin123 <pilarchadwin@gmail.com>
2026-03-26 23:30:25 -0400

    Actualizacion automatica desde Web Admin

--- public/asset/js/activity_links.js
+++ public/asset/js/activity_links.js
@@ -12,12 +12,13 @@ export const activityLinks = {
     "Actividad 3.1": "https://drive.google.com/drive/folders/10baAurbsLPJr6hDeFrPjg43FrTDPqGiM?usp=sharing",
     "Actividad 3.2": "https://drive.google.com/drive/folders/1qw7dNbCKp3h5Y_bHBVx7MJ6uscHYDpRw?usp=sharing",
     "Actividad 3.3": "https://drive.google.com/drive/folders/1uQHD0r2qiHTQyqXs-NsdhAbxoFkwqA6Z?usp=sharing",
-    "Actividad 4": "#",
-    "Actividad 5": "#",
-    "Actividad 6.1": "#",
-    "Actividad 6.2": "#",
-    "Actividad 7": "#",
-    "Actividad 8": "#",
-    "Actividad 9.1": "#",
-    "Actividad 9.2": "#"
+    "Actividad 4": "https://drive.google.com/drive/folders/1nEHrBbuGvcI2XB_q2w09nGa7Y9k4n1Q9?usp=sharing#",
+    "Actividad 5": "https://drive.google.com/drive/folders/1PbWWqgKDBDorh525uecKaGZD21FGSoCeR?usp=sharing",
+    "Actividad 6.1": "https://drive.google.com/drive/folders/1PbWWqgKDBDorh525uecKaGZD21FGSoCeR?usp=sharing",
+    "Actividad 6.2": "https://drive.google.com/drive/folders/1C6T1MPujGTx5kQO74xDxrz1Hiam-kLCm?usp=sharing",
+    "Actividad 7": "https://drive.google.com/drive/folders/1fGWKZpP0oF6mMoJesb-aNO8rGhlIJ9W1?usp=sharing",
+    "Actividad 8": "https://drive.google.com/drive/folders/1R2duphYHC_HO02NVhiQDLFhKx_uc9q-k?usp=sharing",
+    "Actividad 9.1": "https://drive.google.com/drive/folders/1p4hoTMorKr_ZTf3D93HWfTbZMELkw3P3?usp=sharing",
+    "Actividad 9.2": "https://drive.google.com/drive/folders/1L__gQd8SLBcVwm-RCXRSrRIJo0NY7d4j?usp=sharing",
+    "Todas las tareas de límites": "https://drive.google.com/drive/folders/1PbWWqgKDBDorh525uecKaGZD21FGSoCeR_o?usp=sharing"
 };
